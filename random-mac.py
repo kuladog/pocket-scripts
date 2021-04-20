c93c71ddd9627f1e1aea903942e5a4935b06e914
kuladog <2coffees1banana@gmail.com>
2021-04-20 08:33:41 -0700

    fwegr

--- random-mac.py
+++ random-mac.py
@@ -2,7 +2,7 @@
 
 import random
 
-# generates valid unicast UAA hardware address
+# generates a valid unicast UAA hardware address
 def random_mac():
     return "%02x:%02x:%02x:%02x:%02x:%02x" % (
         (random.randint(0, 255) & 0xfc & ~2),
